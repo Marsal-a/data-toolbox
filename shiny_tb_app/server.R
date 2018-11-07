6258419dd0eeafe73b4d4fc773fe7b83eb8af03e
Marsal <a.marsal@manitou-group.com>
2018-11-07 08:31:28 +0100

    udapte

--- shiny_tb_app/server.R
+++ shiny_tb_app/server.R
@@ -1,65 +1,73 @@
 source("ui.R")
 lancement=F
-options(shiny.maxRequestSize = 500*1024^2)
+options(shiny.maxRequestSize = 1000*1024^2)
 server=function(session,input,output){
-  input_data=reactive({
+  
+  MR_input_data=reactive({
     # browser()
-    if(is.null(input$data_input)) return(NULL)
-    infile=input$data_input
-    dt=fread(infile$datapath,encoding = "Latin-1",check.names = T)
+    if(is.null(input$MR_data_input)) return(NULL)
+    infile=input$MR_data_input
+    
+    if(grepl(".Rdata",infile$datapath)){
+      l=load(infile$datapath)
+      dt=get(l)
+    }else{
+      dt=fread(infile$datapath,encoding = "Latin-1",check.names = T)  
+    }
+    
     return(dt)
   })
-  output$data_loaded <- renderDataTable({
-    if(is.null(input$col_to_match)) return(NULL)
-    if(input$col_to_match=="") return(NULL)
-    col=eval(input$col_to_match)
-    DT=datatable(input_data()[,..col],options = list(
+  output$MR_data_loaded <- renderDataTable({
+    if(is.null(input$MR_col_to_match)) return(NULL)
+    if(input$MR_col_to_match=="") return(NULL)
+    col=eval(input$MR_col_to_match)
+    # browser()
+    MR_input_data()[]
+    DT=datatable(unique(MR_input_data()[,col,with=F]),options = list(
       autoWidth = TRUE,
       dom="ftp",
       pageLength = 25,
       columnDefs = list(list(width = '200%', targets = "_all"))
-      ),rownames = F)%>%
+    ),rownames = F)%>%
       formatStyle( 0, target= 'row', lineHeight='50%')
     return(DT)
+  },server = T)
+  MR_col_data_to_match_selection=reactive({
+    cols=names(MR_input_data())
   })
-  col_data_to_match_selection=reactive({
-    cols=names(input_data())
-  })
-  output$col_to_match_selection=renderUI({
-    if(is.null(input$data_input)) return(NULL)
-    selectInput("col_to_match","Colonne à matcher ? ",choices=c(col_data_to_match_selection()))
+  output$MR_col_to_match_selection=renderUI({
+    if(is.null(input$MR_data_input)) return(NULL)
+    selectInput("MR_col_to_match","Colonne à matcher ? ",choices=c(MR_col_data_to_match_selection()))
   })
   
-  
-  input_ref=reactive({
-  
-    if(is.null(input$referential_input)) return(NULL)
-    infile=input$referential_input
-    dt=fread(infile$datapath,encoding = "Latin-1",check.names = T)
+  MR_input_ref=reactive({
+    if(is.null(input$MR_referential_input)) return(NULL)
+    infile=input$MR_referential_input
+    if(grepl(".Rdata",infile$datapath)){
+      l=load(infile$datapath)
+      dt=get(l)
+    }else{
+      dt=fread(infile$datapath,encoding = "Latin-1",check.names = T)  
+    }
     return(dt)
   })
-  output$ref_loaded <- renderDataTable({
-    if(is.null(input$col_matched)) return(NULL)
-    if(input$col_matched=="") return(NULL)
-    col=eval(input$col_matched)
-    DT=datatable(input_ref()[,..col],options = list(autoWidth = TRUE,dom="ftp", pageLength = 25),rownames = F)%>%
+  output$MR_ref_loaded <- renderDataTable({
+    if(is.null(input$MR_col_matched)) return(NULL)
+    if(input$MR_col_matched=="") return(NULL)
+    col=eval(input$MR_col_matched)
+    DT=datatable(unique(MR_input_ref()[,col,with=F]),options = list(autoWidth = TRUE,dom="ftp", pageLength = 25),rownames = F)%>%
       formatStyle( 0, target= 'row', lineHeight='50%')
     return(DT)
   })
-  col_referential_to_be_matched=reactive({
-    cols=names(input_ref())
+  MR_col_referential_to_be_matched=reactive({
+    cols=names(MR_input_ref())
   })
-  output$col_matched=renderUI({
-    if(is.null(input$referential_input)) return(NULL)
-    selectInput("col_matched","Colonne du référentiel à utiliser?",choices=col_referential_to_be_matched())
+  output$MR_col_matched=renderUI({
+    if(is.null(input$MR_referential_input)) return(NULL)
+    selectInput("MR_col_matched","Colonne du référentiel à utiliser?",choices=MR_col_referential_to_be_matched())
   })
   
-  match_ref_model=function(data_in,
-                           ref_in,
-                           col_name_ref_model,
-                           col_data_to_match,
-                           max_dist=NULL,
-                           string_dist_method){
+  MR_match_ref_model=function(data_in,ref_in,col_name_ref_model,col_data_to_match,max_dist=NULL,string_dist_method){
     
     library(stringdist)
     # browser()
@@ -68,16 +76,16 @@ server=function(session,input,output){
     
     #calcualte best matched model
     match_ref_model=CJ(data_unique=list_data_unique,ref_model=list_ref_model)
-    if(input$method=="qgram"){
-      match_ref_model[,dist:=stringdist(data_unique,ref_model,method ="qgram",q=input$param)]  
+    if(string_dist_method=="qgram"){
+      match_ref_model[,dist:=stringdist(data_unique,ref_model,method ="qgram",q=input$MR_param)]  
     }
-    if(input$method=="lv"){
+    if(string_dist_method=="lv"){
       match_ref_model[,dist:=stringdist(data_unique,ref_model,method ="lv")]  
     }
-    if(input$method=="jw"){
+    if(string_dist_method=="jw"){
       match_ref_model[,dist:=stringdist(data_unique,ref_model,method ="jw")]  
     }
-    setorder(match_ref_model,data_unique,dist)
+    setorder(match_ref_model,data_unique,dist,na.last = T)
     
     #keep only matching for distance 
     if(!is.null(max_dist)){
@@ -95,29 +103,138 @@ server=function(session,input,output){
     
   }
   
-  dt_out_reac=eventReactive(input$run_matching,{
+  MR_dt_out_reac=eventReactive(input$MR_run_matching,{
     
-    dt_out=match_ref_model(input_data(),input_ref(),input$col_matched,input$col_to_match)
+    dt_out=MR_match_ref_model(MR_input_data(),MR_input_ref(),input$MR_col_matched,input$MR_col_to_match,NULL,input$MR_method)
     showModal(modalDialog(title = "","matching ok",easyClose = TRUE,footer = tagList(modalButton("Ok"))))
     return(dt_out)
     
   })
-  output$res=renderDataTable({
-    cols=c(eval(input$col_to_match),"ref_model")
-    dt=dt_out_reac()[,..cols]
+  output$MR_res=renderDataTable({
+    cols=c(eval(input$MR_col_to_match),"ref_model")
+    dt=unique(MR_dt_out_reac()[,..cols])
     DT=datatable(dt,options = list(autoWidth = F,dom="ftp", pageLength = 25),rownames = F)%>%
       formatStyle( 0, target= 'row', lineHeight='50%')
     return(DT)
   })
   
-  output$downloadData <- downloadHandler(
+  output$MR_downloadData <- downloadHandler(
     filename = function() {
       # browser()
-      paste(gsub("\\..*","",input$data_input$name),"_matched",".csv", sep = "")
+      paste(gsub("\\..*","",input$MR_data_input$name),"_matched",".csv", sep = "")
     },
     content = function(file) {
-      fwrite(dt_out_reac(), file, row.names = FALSE)
+      fwrite(MR_dt_out_reac(), file, row.names = FALSE)
     }
+  )
+  
+  HE_input_data=reactive({
+    # browser()
+    if(is.null(input$HE_file_input)) return(NULL)
+    infile=input$HE_file_input
+    dt=fread(infile$datapath,encoding = "UTF-8",check.names = T)
+    return(dt)
+  })
+  HE_col_names=reactive({
+    cols=c("",names(HE_input_data()))
+  })
+  output$HE_input_1=renderUI({
+    if(is.null(input$HE_file_input)) return(NULL)
+    selectInput("HE_col_1","input 1",HE_col_names())
+  })
+  output$HE_input_2=renderUI({
+    if(is.null(input$HE_file_input)) return(NULL)
+    selectInput("HE_col_2","input 1",HE_col_names())
+  })
+  output$HE_input_3=renderUI({
+    if(is.null(input$HE_file_input)) return(NULL)
+    selectInput("HE_col_3","input 3",HE_col_names())
+  })
+  output$HE_input_4=renderUI({
+    if(is.null(input$HE_file_input)) return(NULL)
+    selectInput("HE_col_4","input 4",HE_col_names())
+  })
+  output$HE_input_5=renderUI({
+    if(is.null(input$HE_file_input)) return(NULL)
+    selectInput("HE_col_5","input 5",HE_col_names())
+  })
+  
+  sankey_diagram_plot=function(dt,columns,prefixes=c("c1_","c2_","c3_","c4_","c5_","c6_"),removeNAs=T){
+    
+    # browser()
+    levels=length(columns)-1
+    links=rbindlist(map(1:levels,function(x){
+      res=dt[,.N,by=.(source=get(columns[x]),target=get(columns[x+1]))][order(source,target)]
+      if(removeNAs){
+        res=res[!is.na(source) & !is.na(target)]
+        res=res[source!="" & target!=""]
+      }
+      res[,source:=paste0(prefixes[x],source)]
+      res[,target:=paste0(prefixes[x+1],target)]
+    }))
+    
+    # if(input$HE_tri){
+      links[,pos1:=1:.N-1]
+      links[,pos2:=(.N):(2*.N-1)]
+      nodes=rbind(links[,.(names=source,pos=pos1)],links[,.(names=target,pos=pos2)])
+      nodes=nodes[order(pos)]
+      nodes=unique(nodes,by="names")
+      nodes=nodes[order(pos)]
+      nodes[,pos:=1:.N-1]
+      
+    # }else{
+    #   nodes=rbind(links[,.(names=source)],links[,.(names=target)])
+    #   nodes=nodes[order(names),.(names=unique(names))]
+    #   nodes[,pos:=1:.N-1]
+    #   nodes=nodes[order(pos)]
+    # }
+    
+    setkey(links,source)
+    setkey(nodes,names)
+    links[nodes,int_source:=i.pos]
+    setkey(links,target)
+    links[nodes,int_target:=i.pos]
+    nodes=nodes[order(pos)]
+    nodes[,names:=gsub("c[0-9]_","",names)]
+    
+    sk=sankeyNetwork(Links=links,Nodes=nodes,
+                     Source = "int_source",Target = "int_target",Value="N",
+                     NodeID = "names",sinksRight = F,
+                     units = " machines",fontSize = 12, nodeWidth = 30,
+                     iterations = 500
+                     # iterations = input$HE_iteration
+                     )
+    
+    return(sk)
+  }
+  
+  output$HE_sankey_plot=renderSankeyNetwork({
+    
+    cols=c(input$HE_col_1,input$HE_col_2,input$HE_col_3,
+           input$HE_col_4,input$HE_col_5,input$HE_col_6)
+    cols=cols[which(!cols=="")]
+    if(length(cols)<2){
+      res=NULL
+    }else{
+      res=sankey_diagram_plot(HE_input_data(),columns = cols,removeNAs =F)  
+    }
+    
+    return(res)
+  })
+  
+  HE_sankey_height=reactive({
+    # browser()
+    cols=c(input$HE_col_1,input$HE_col_2,input$HE_col_3,
+           input$HE_col_4,input$HE_col_5,input$HE_col_6)
+    cols=cols[which(!cols=="")]
+    if(is.null(cols)) return(NULL)
+    nrows=max(unlist(map(cols,function(x) length(HE_input_data()[,unique(get(x))]))))
+    return(nrows)
+  })
+  
+  output$HE_sankey_ui=renderUI(
+    sankeyNetworkOutput("HE_sankey_plot",height = paste0("",max(HE_sankey_height()*15,500),"px",""))
+    # sankeyNetworkOutput("HE_sankey_plot",height = "500px")
   )
   
 }
